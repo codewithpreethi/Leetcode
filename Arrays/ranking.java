8cf3ee247a160bf073c726f3413db3d13ef95e95
Preethi <preethivelu90@gmail.com>
2026-05-20 19:52:52 +0530

    Rank tranform problem

---
+++ Arrays/ranking.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        if(arr.length==0){
+            return arr;
+        }
+        int copy[]=new int[arr.length];
+        
+        for(int i=0;i<arr.length;i++){
+            copy[i]=arr[i];
+        }
+        Arrays.sort(copy);
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int rank=1;
+        for(int i=0;i<copy.length;i++){
+        if(!map.containsKey(copy[i])){
+            map.put(copy[i],rank);
+            rank++;
+        }
+        }
+    
+    for(int i=0;i<arr.length;i++){
+        arr[i]=map.get(arr[i]);
+    }return arr;
+    }
+}
